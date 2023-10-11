c5b340f76eac7781d87996246b4b094493d1564d
Lagger <102471956+realLaggermaster@users.noreply.github.com>
2023-10-11 22:19:48 +0300

    Create Basic lua function script.lua

---
+++ Basic lua function script.lua
@@ -0,0 +1,20 @@
+-- Variables
+apple = 2
+pear = 3
+
+function checkFunction() --Basic Lua function scrip.
+    if apple == 2 then
+        print('We have 2 apples yes.')
+    else
+        print('there isnt 2 apples, we have:', apple)
+    end
+
+    if pear == 2 then
+        print('we have 2 pears.')
+    else 
+        print('We dont have 2 pears, we have:', pear)
+    end
+
+end
+
+checkFunction() -- Gives the function a task.
